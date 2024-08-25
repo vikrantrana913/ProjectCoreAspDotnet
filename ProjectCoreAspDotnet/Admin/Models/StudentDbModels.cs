731e786fdd5d963d687b354d2e1604510f29373c
Vikrant Singh <vikrantrana913@gmail.com>
2024-08-26 05:18:46 +0530

    adding files

---
+++ ProjectCoreAspDotnet/Admin/Models/StudentDbModels.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectCoreAspDotnet.Admin.Models
+{
+    public class StudentDbModels
+    {
+        public int StudentId { get; set; }
+
+        [Display(Name ="Fullname")]
+        [Required(ErrorMessage ="The field is required.")]
+        public int Fullname { get; set; }
+
+        [Display(Name ="Date of Birth")]
+        [Required(ErrorMessage ="The field is required.")]
+        public DateTime Dob { get; set; }
+
+        [Display(Name ="Gender")]
+        [Required(ErrorMessage ="The field is required.")]
+        public string Gender { get; set; }
+
+        [Display(Name ="Address")]
+        [Required(ErrorMessage ="The field is required.")]
+        public string Address { get; set; }
+
+        [Display(Name ="ContactNumber")]
+        [Required(ErrorMessage ="The field is required.")]
+        public string ContactNumber { get; set; }
+
+        [Display(Name ="Email")]
+        [Required(ErrorMessage ="The field is required.")]
+        public string Email { get; set; }
+
+    }
+}
